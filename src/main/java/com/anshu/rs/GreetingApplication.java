697690ae0c7c1a590663ceb21f835cc8977a6d71
anshuman <anshuman18@gmail.com>
2019-08-25 11:32:10 +0530

    remove sysouts

--- src/main/java/com/anshu/rs/GreetingApplication.java
+++ src/main/java/com/anshu/rs/GreetingApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreetingApplication {
 
   public static void main(String[] args) {
-    System.err.println("GreetingApplication init");
-    System.out.println("GreetingApplication hahahha");
     SpringApplication.run(GreetingApplication.class, args);
   }
 }
